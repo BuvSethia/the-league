b1ac7624e101b29a89609573ee5e94b3012de3a6
Sumbhav Sethia <sumbhav.sethia@gmail.com>
2024-04-18 04:01:10 -0700

    Add load setup

--- db/setup.js
+++ db/setup.js
@@ -2,10 +2,12 @@ const fs = require("fs");
 const initSqlJs = require("sql.js");
 
 const tableStatements = fs.readFileSync(`${__dirname}/tables.sql`, { encoding: 'utf-8' });
+const loadStatements = fs.readFileSync(`${__dirname}/load.sql`, { encoding: 'utf-8' });
 
 initSqlJs().then(function (SQL) {
   const db = new SQL.Database();
   db.run(tableStatements)
+  db.run(loadStatements)
 
   fs.writeFileSync(`${__dirname}/db.sqlite`, Buffer.from(db.export()));
 });
