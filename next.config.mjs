a38513db9e6681646cba33a53ffd9403c0f00dc4
Sumbhav Sethia <sumbhav.sethia@gmail.com>
2024-04-18 07:28:58 -0700

    update base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/the-league",
     output: 'export',
     distDir: 'docs',
     webpack: (config, { isServer, webpack }) => {
