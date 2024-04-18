c504277dcb76cf5be4e2a4591924b16be3059bdc
Sumbhav Sethia <sumbhav.sethia@gmail.com>
2024-04-18 16:59:36 -0700

    works?

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,18 @@
 const nextConfig = {
     basePath: "/the-league",
     output: 'export',
-    webpack: (config, { isServer, webpack }) => {
-        if (!isServer) {
-            config.resolve.fallback.fs = false;
-        }
+    webpack: (config, { webpack }) => {
+        config.resolve.fallback = { fs: false };
 
         config.plugins.push(
             new webpack.IgnorePlugin({
-            checkResource(resource, context) {
-                if (resource.includes('/db/') && !context.includes('node_modules')) {
-                return true;
-                }
-                return false;
-            },
+                checkResource(resource, context) {
+                    if (resource.includes('/db/') && !context.includes('node_modules')) {
+                        return true;
+                    }
+                    
+                    return false;
+                },
             }),
         );
 
